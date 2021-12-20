61c55af04eb66322fb99032c3cc0078f38463596
AI-Gio <wreckitsnoofs@gmail.com>
2021-12-20 13:07:45 +0100

    Added epsilon decay and params to load model

--- src/main.py
+++ src/main.py
@@ -3,24 +3,42 @@ import agent, transition  # , memory, function_approx
 import numpy as np
 import matplotlib.pyplot as plt
 env = gym.make('LunarLander-v2')
+np.random.seed(7)
 
 
-# TODO: ↓ maak hier een functie van
-
-def main(n_epi: int, steps: int, n_train: int, n_update: int):
+def main(n_epi: int, steps: int, n_train: int, n_update: int, n_decay: int, decay_r: float, load_m: bool):
     """
     :param steps: maximum amount of steps an episode is allowed to take
     :param n_epi: amount of episodes
     :param n_train: after n times train policy network
     :param n_update: after n steps update target network
+    :param n_decay: after n episodes
+    :param decay_r: the rate of how much epsilon decreases
     :return:
     """
-    rewards = []
+    rewards = np.array([])
     count = 1  # counter across all episodes
+
+    if load_m:
+        agent.policy_network.load_network("policy_network")
+        agent.target_network.load_network("target_network")
+
     for i_episode in range(n_epi):
         current_state = env.reset()
+        print(f"n episodes: {i_episode}")
+
+        # Safe network after 25 episodes
+        if i_episode % 25 == 0 and i_episode > 1:
+            print("Saved networks")
+            agent.policy_network.save_network("policy_network")
+            agent.target_network.save_network("target_network")
+
+        # Epsilon decay
+        if i_episode % n_decay == 0:
+            agent.epsilon *= decay_r
+            print(f"agent epsilon: {agent.epsilon}")
+
         for step in range(steps):
-            print(count)
             env.render()
 
             # Train policy network
@@ -28,14 +46,18 @@ def main(n_epi: int, steps: int, n_train: int, n_update: int):
                 agent.train()
 
             # Update target network
-            if count % n_update == 0:
+            if count % n_update == 0 and i_episode > 1:
+                print(count)
                 agent.update_t_network()  # TODO: kan nog iets minder vak dan elke train
+
+            if count % 2000 == 0:
+                print(f"rewards freq ^ 50: {len([i for i in rewards if i > 50])}")
 
             # Select_action using policy network in agent.py
             action = agent.choose_action(current_state)  # env.action_space.sample()
             next_state, reward, done, info = env.step(action)  # next_state is observation
 
-            rewards.append(reward)
+            rewards = np.append(rewards, reward)
 
             # Save transition in memory of agent
             t = transition.Transition(current_state, action, reward, next_state, done)
@@ -45,8 +67,10 @@ def main(n_epi: int, steps: int, n_train: int, n_update: int):
             count += 1
 
             if done:
+                # agent.train()
                 print("Episode finished after {} timesteps".format(step + 1))
                 break
+
     agent.policy_network.save_network("policy_network")
     agent.target_network.save_network("target_network")
     env.close()
@@ -55,11 +79,12 @@ def main(n_epi: int, steps: int, n_train: int, n_update: int):
     plt.ylabel("reward over time")
     plt.show()
 
+# bron: https://medium.datadriveninvestor.com/training-the-lunar-lander-agent-with-deep-q-learning-and-its-variants-2f7ba63e822c
 
-epsilon, discount = [0.1, 0.9]
-epochs, batch_size, learning_rate, tau = [5, 32, 0.001, 0.7]
-memory_size = 1000
+epsilon, discount = [0.5, 0.9]  # begin epsilon: 0.5
+epochs, batch_size, learning_rate, tau = [1, 64, 0.001, 0.7]
+memory_size = 10000
 
 agent = agent.Agent(discount=discount, epsilon=epsilon, tau=tau, batch_size=batch_size,
                     epochs=epochs, memory_size=memory_size, learning_rate=learning_rate)
-main(20, 200, 100, 400)
+main(n_epi=800, steps=300, n_train=75, n_update=20, n_decay=1, decay_r=0.995, load_m=False)
